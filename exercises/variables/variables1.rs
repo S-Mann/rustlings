7449a437ba595bb409586a2f0867a221a5de4f60
S-Mann <sukhinder.mann@gmail.com>
2021-04-18 21:35:26 +0100

    ✨ completed variables assignment

--- exercises/variables/variables1.rs
+++ exercises/variables/variables1.rs
@@ -1,14 +1,17 @@
-// variables1.rs
-// Make me compile! Execute the command `rustlings hint variables1` if you want a hint :)
-
-// About this `I AM NOT DONE` thing:
-// We sometimes encourage you to keep trying things on a given exercise,
-// even after you already figured it out. If you got everything working and
-// feel ready for the next exercise, remove the `I AM NOT DONE` comment below.
-
-// I AM NOT DONE
-
 fn main() {
-    x = 5;
-    println!("x has the value {}", x);
+    /* Variables can be mutable(the variable can be reassigned with new value) or immutable */
+    let x = 5;
+    let mut mut_x = 10;
+    /*
+    You can also declare constants but they can only be immutable and can have global and local scope. const keyword can only be use on experssions that have evaluate to constant values. So no result from functions and other run time values. Constants also need to have explicit data typing.
+    */
+    const APP_NAME: &str = "my_awesome_app";
+    /*
+    We can also do something called as variable shadowing, which is like reinitializing a variable in the same scope. This is reminiscent of most interpreted languages where you do
+    x = 10;
+    x = "string";
+    */
+    let y = "text";
+    let x = y;
+    println!("The value of x is: {}", x);
 }
