dff3623c1e3b403891d872195506e32a54dc8c95
S-Mann <sukhinder.mann@gmail.com>
2021-04-20 22:27:40 +0100

    ✨ added solutions for functions exercises

--- exercises/functions/functions3.rs
+++ exercises/functions/functions3.rs
@@ -1,13 +1,11 @@
-// functions3.rs
-// Make me compile! Execute `rustlings hint functions3` for hints :)
-
-// I AM NOT DONE
-
 fn main() {
-    call_me();
+    call_me(3);
 }
 
 fn call_me(num: u32) {
+    // !: Rust doesn't allow default arguments for functions
+    // https://www.reddit.com/r/rust/comments/fi6nov/why_does_rust_not_support_default_arguments/
+    // https://stackoverflow.com/questions/24047686/default-function-arguments-in-rust
     for i in 0..num {
         println!("Ring! Call number {}", i + 1);
     }
