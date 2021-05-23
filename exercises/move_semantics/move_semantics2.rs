05eaa79bc6478f156381b508430cdfed79250bfc
S-Mann <sukhinder.mann@gmail.com>
2021-05-23 21:10:28 +0100

    ✨ added ownership exercises solutions

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -1,28 +1,25 @@
-// move_semantics2.rs
-// Make me compile without changing line 13!
-// Execute `rustlings hint move_semantics2` for hints :)
-
-// I AM NOT DONE
+/*
+?: There are 3 ways to solve this
+1. Call clone() on vec0 in main(), which will do a copy in heap.
+2. Pass vec0 to fill_vec() and call clone() there.
+3. Make vec0 mutable pass mutable reference to fill_vec() and completely remove vec1.
+*/
 
 fn main() {
-    let vec0 = Vec::new();
+    let mut vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    fill_vec(&mut vec0);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
 
-    vec1.push(88);
+    vec0.push(88);
 
-    println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+    println!("{} has length {} content `{:?}`", "vec1", vec0.len(), vec0);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
-
+fn fill_vec(vec: &mut Vec<i32>) {
     vec.push(22);
     vec.push(44);
     vec.push(66);
-
-    vec
 }
