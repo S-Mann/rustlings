1173d6065a380fa9e3c1ea673b8de8d2e6de8dad
S-Mann <sukhinder.mann@gmail.com>
2021-07-21 14:55:16 +0100

    ✨ completed generics exercise

--- exercises/generics/generics3.rs
+++ exercises/generics/generics3.rs
@@ -1,29 +1,72 @@
-// An imaginary magical school has a new report card generation system written in Rust!
-// Currently the system only supports creating report cards where the student's grade
-// is represented numerically (e.g. 1.0 -> 5.5).
-// However, the school also issues alphabetical grades (A+ -> F-) and needs
-// to be able to print both types of report card!
+pub trait Grade {
+    fn print(&self) -> String;
+}
 
-// Make the necessary code changes in the struct ReportCard and the impl block
-// to support alphabetical report cards. Change the Grade in the second test to "A+"
-// to show that your changes allow alphabetical grades.
-
-// Execute 'rustlings hint generics3' for hints!
-
-// I AM NOT DONE
-
-pub struct ReportCard {
-    pub grade: f32,
+pub struct ReportCard<T> {
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
-    pub fn print(&self) -> String {
-        format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+impl Grade for ReportCard<f32> {
+    fn print(&self) -> String {
+        format!(
+            "{} ({}) - achieved a grade of {}",
+            &self.student_name, &self.student_age, &self.grade
+        )
     }
 }
+impl Grade for ReportCard<&str> {
+    fn print(&self) -> String {
+        format!(
+            "{} ({}) - achieved a grade of {}",
+            &self.student_name, &self.student_age, &self.grade
+        )
+    }
+}
+
+/* //This is better because you can allow any data type for grade that implements 'Display' trait, pay attention as here
+use std::fmt::Display;
+
+
+pub struct ReportCard<T: Display> {
+    pub grade: T,
+    pub student_name: String,
+    pub student_age: u8,
+}
+
+impl <T: Display> ReportCard<T> {
+    fn print(&self) -> String {
+        format!(
+            "{} ({}) - achieved a grade of {}",
+            &self.student_name, &self.student_age, &self.grade
+        )
+    }
+}
+
+fn main() {
+    let report_card = ReportCard {
+        grade: 2.1,
+        student_name: "Tom Wriggle".to_string(),
+        student_age: 12,
+    };
+    println!(
+        "{} == {}",
+        report_card.print(),
+        "Tom Wriggle (12) - achieved a grade of 2.1"
+    );
+    let report_card = ReportCard {
+        grade: "A+",
+        student_name: "Gary Plotter".to_string(),
+        student_age: 11,
+    };
+    println!(
+        "{} == {}",
+        report_card.print(),
+        "Gary Plotter (11) - achieved a grade of A+"
+    );
+}
+ */
 
 #[cfg(test)]
 mod tests {
@@ -44,9 +87,8 @@ mod tests {
 
     #[test]
     fn generate_alphabetic_report_card() {
-        // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: "A+",
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
