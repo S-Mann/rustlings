1e2e59d4e7c2b01e7af2fd73413f4e89e8fbe868
S-Mann <sukhinder.mann@gmail.com>
2021-08-02 23:14:21 +0100

    ✨ completed tests exercise

--- exercises/tests/tests1.rs
+++ exercises/tests/tests1.rs
@@ -1,17 +1,27 @@
-// tests1.rs
-// Tests are important to ensure that your code does what you think it should do.
-// Tests can be run on this file with the following command:
-// rustlings run tests1
-
-// This test has a problem with it -- make the test compile! Make the test
-// pass! Make the test fail! Execute `rustlings hint tests1` for hints :)
-
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert() {
-        assert!();
+        assert!(true);
+    }
+
+    #[test]
+    fn you_can_compare() {
+        assert_eq!(1 + 1, 2);
+    }
+
+    #[test]
+    #[should_panic]
+    fn you_can_panic() {
+        panic!("Trouble!");
+    }
+
+    #[test]
+    fn you_can_result() -> Result<(), String> {
+        if 1 + 1 == 2 {
+            Ok(())
+        } else {
+            Err(String::from("Equality broken."))
+        }
     }
 }
